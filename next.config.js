023e70c8723e9260d0133c81112a79bcdaf82067
mujehoxe <mujehoxe@gmail.com>
2024-09-02 16:02:17 +0100

    vercel cors

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
+  images: {
     domains: ["216.10.242.11", "65.20.87.36"],
   },
-};
-
-    },
 };
 
 module.exports = nextConfig;
