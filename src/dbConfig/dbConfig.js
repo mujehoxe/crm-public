347cca45bc4259d5862d3adfdd393115d34c1f52
mujehoxe <mujehoxe@gmail.com>
2024-09-02 19:34:23 +0100

    mongo_url to MONGO_URL

--- src/dbConfig/dbConfig.js
+++ src/dbConfig/dbConfig.js
@@ -1,24 +1,20 @@
 import mongoose from "mongoose";
 
+async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URL);
+    const connection = mongoose.connection;
+    connection.setMaxListeners(15);
+    connection.on("connected", () => {
+      console.log("connected");
+    });
+    connection.on("error", (err) => {
+      console.log(err);
+      process.exit(1);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
 
-  async function connect(){
-
-    try{
-        mongoose.connect(process.env.mongo_url);
-        const connection = mongoose.connection;
-        connection.setMaxListeners(15);
-        connection.on('connected',() => {
-            console.log("connected");
-        })
-        connection.on('error',(err) => {
-            console.log(err);
-            process.exit(1);
-        })
-
-    }
-    catch(e){
-        console.log(e);
-    }
- }
-
- export default connect;
+export default connect;
