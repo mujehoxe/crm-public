c47167b364dda370ef424ba927ca01de3453ebe6
mujehoxe <mujehoxe@gmail.com>
2024-09-02 13:41:10 +0100

    revert to baseurl

--- src/app/components/topbar.js
+++ src/app/components/topbar.js
@@ -123,7 +123,7 @@ const Topbar = ({
               {avtaar != null ? (
                 <div className="overflow-hidden">
                   <img
-                    src={"http://65.20.87.36:3333" + avtaar}
+                    src={process.env.NEXT_PUBLIC_BASE_URL + avtaar}
                     className="tablet:w-[35px] mobile:w-[25px]"
                   />
                 </div>
