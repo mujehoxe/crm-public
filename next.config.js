9de5689fd61f5ece0c76fbee5609c9b7e42275c3
mujehoxe <mujehoxe@gmail.com>
2024-09-02 13:46:19 +0100

    added domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['216.10.242.11'],
+    domains: ["216.10.242.11", "65.20.87.36"],
+  },
+};
+
     },
 };
 
