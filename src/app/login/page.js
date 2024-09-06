4f7ce7caff1ecc09c7378082450055576deaa051
mujehoxe <mujehoxe@gmail.com>
2024-09-06 05:47:25 +0100

    Miles colors on login btn

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -102,7 +102,7 @@ export default function LoginPage() {
               <div>
                 <button
                   type="submit"
-                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-[#176298] hover:bg-[#176266] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#176298]"
                   disabled={loading}
                 >
                   {loading ? (
