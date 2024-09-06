07bc1d22f216d083fe356ff595493dd991b8a545
mujehoxe <mujehoxe@gmail.com>
2024-09-06 01:35:32 +0100

    settings btn on side bar (not working)

--- src/app/components/sidebar.js
+++ src/app/components/sidebar.js
@@ -14,10 +14,10 @@ import {
   ArrowLeftStartOnRectangleIcon,
   Bars3Icon,
   ClipboardDocumentCheckIcon,
+  Cog6ToothIcon,
   HomeIcon,
   UserGroupIcon,
 } from "@heroicons/react/24/outline";
-import { useRouter } from "next/router";
 import { toast } from "react-toastify";
 
 const Sidebar = ({ sidePanelStat, setSidePanelStat, buttonRef }) => {
@@ -53,13 +53,9 @@ const Sidebar = ({ sidePanelStat, setSidePanelStat, buttonRef }) => {
     if (sideMenus[index].nested) {
       setSidePanelStat(true);
     }
-    if (index == 5) {
-      handleLogout();
-    }
   };
   const divRef = useRef(null);
 
-  // Effect to add and remove click outside event listener
   const handleClickOutside = (event) => {
     if (
       divRef.current &&
@@ -88,14 +84,14 @@ const Sidebar = ({ sidePanelStat, setSidePanelStat, buttonRef }) => {
       id: 0,
       name: "profile",
       link: "/profile",
-      icon: <HomeIcon className="ml-3 w-6 text-gray-400" />,
+      icon: <HomeIcon className="ml-3 w-6" />,
       visibility: ["all"],
     },
     {
       id: 1,
       name: "staff",
       link: "/Staff",
-      icon: <UserGroupIcon className="ml-3 w-6 text-gray-400" />,
+      icon: <UserGroupIcon className="ml-3 w-6" />,
       notifications: 5,
       visibility: ["hr", "admin", "superadmin"],
     },
@@ -103,7 +99,7 @@ const Sidebar = ({ sidePanelStat, setSidePanelStat, buttonRef }) => {
       id: 2,
       name: "leads",
       link: "",
-      icon: <IoGitNetworkSharp className="ml-3 w-6 text-gray-400" />,
+      icon: <IoGitNetworkSharp className="ml-3 w-6" />,
       nested: [
         {
           name: "Community Leads",
@@ -137,7 +133,7 @@ const Sidebar = ({ sidePanelStat, setSidePanelStat, buttonRef }) => {
       id: 3,
       name: "reports",
       link: "",
-      icon: <ClipboardDocumentCheckIcon className="ml-3 w-6 text-gray-400" />,
+      icon: <ClipboardDocumentCheckIcon className="ml-3 w-6" />,
       nested: [
         {
           name: "Timesheet",
@@ -183,7 +179,7 @@ const Sidebar = ({ sidePanelStat, setSidePanelStat, buttonRef }) => {
       id: 4,
       name: "your deals",
       link: "",
-      icon: <FaHandshake className="ml-3 w-6 text-gray-400" />,
+      icon: <FaHandshake className="ml-3 w-6" />,
       nested: [
         {
           name: "Deals Approvals",
@@ -224,15 +220,6 @@ const Sidebar = ({ sidePanelStat, setSidePanelStat, buttonRef }) => {
         "finance",
       ],
     },
-
-    {
-      id: 5,
-      name: "Log out",
-      icon: (
-        <ArrowLeftStartOnRectangleIcon className="ml-3 w-6 text-gray-400" />
-      ),
-      visibility: ["all"],
-    },
   ];
 
   return (
@@ -242,16 +229,14 @@ const Sidebar = ({ sidePanelStat, setSidePanelStat, buttonRef }) => {
         sidePanelStat ? "w-64" : "w-20"
       }`}
     >
-      <div className="relative flex flex-col h-full overflow-hidden">
+      <div className="flex flex-col h-full">
         <div className="p-2 py-3 bg-gray-800">
           <div
-            onClick={() => {
-              setSidePanelStat(!sidePanelStat);
-            }}
-            className="flex w-full py-2 gap-1 rounded-lg bg-gray-800 text-gray-400 hover:bg-gray-700"
+            onClick={() => setSidePanelStat(!sidePanelStat)}
+            className="flex w-full py-2 gap-1 rounded-lg bg-gray-800 text-gray-400 hover:bg-gray-700 cursor-pointer"
           >
             <MenuButton className="flex px-2 items-center transition-colors duration-200 hover:text-white">
-              <Bars3Icon className={`ml-3 w-6 text-gray-400`} />
+              <Bars3Icon className="ml-3 w-6 text-gray-400" />
             </MenuButton>
             {sidePanelStat && (
               <span className="flex-1 text-xs capitalize align-middle my-auto whitespace-nowrap">
@@ -260,146 +245,118 @@ const Sidebar = ({ sidePanelStat, setSidePanelStat, buttonRef }) => {
             )}
           </div>
         </div>
-        <nav className="flex flex-col flex-grow overflow-y-auto my-2">
-          <ul className="flex flex-1 flex-col space-y-2 px-2 w-full h-full">
-            {sideMenus.map(
-              (item, index) =>
-                (item.visibility?.includes(userrole?.toLowerCase()) ||
-                  item.visibility?.includes("all")) && (
-                  <li
-                    key={item.id}
-                    className={`${item.name === "Log out" && "mt-auto"}`}
-                  >
-                    <Link
-                      href={item.link || "#"}
-                      className={`flex items-center p-2 rounded-lg transition-colors duration-200 ${
-                        pathname === item.link
-                          ? "bg-gray-700 text-white"
-                          : "text-gray-400 hover:bg-gray-700 hover:text-white"
-                      } ${
-                        item.name === "Log out"
-                          ? "hover:bg-red-600 hover:text-white"
-                          : ""
-                      } ${loading ? "opacity-60 cursor-not-allowed" : ""}`}
-                      onClick={() => !loading && sideHandler(index)}
-                    >
-                      <span className="mr-3 text-2xl">{item.icon}</span>
-                      {sidePanelStat && (
-                        <span className="flex-1 text-xs capitalize whitespace-nowrap">
-                          {item.name}
-                        </span>
-                      )}
-                      {sidePanelStat && item.notifications && (
-                        <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-gray-900 bg-blue-200 rounded-full">
-                          {item.notifications}
-                        </span>
-                      )}
-                      {sidePanelStat && item.nested && (
-                        <FaChevronDown
-                          className={`ml-auto transition-transform duration-200 ${
-                            currentIndex === index ? "transform rotate-180" : ""
-                          }`}
-                        />
-                      )}
-                    </Link>
-                    {sidePanelStat && currentIndex === index && item.nested && (
-                      <ul className="pl-6 mt-2 space-y-2">
-                        {item.nested.map(
-                          (subItem, subIndex) =>
-                            subItem.visibility?.includes(
-                              userrole.toLowerCase()
-                            ) && (
-                              <li key={subIndex}>
-                                <Link
-                                  href={subItem.link || "#"}
-                                  className={`block p-2 text-xs rounded-lg transition-colors duration-200 ${
-                                    pathname === subItem.link
-                                      ? "bg-gray-700 text-white"
-                                      : "text-gray-400 hover:bg-gray-700 hover:text-white"
-                                  }`}
-                                >
-                                  {subItem.name}
-                                </Link>
-                              </li>
-                            )
-                        )}
-                      </ul>
-                    )}
-                  </li>
-                )
-            )}
-          </ul>
-        </nav>
-      </div>
 
-      <div className="relative flex flex-col h-screen my-2">
-        <nav className="flex flex-col flex-grow overflow-y-auto">
-          <ul className="space-y-2 px-2 w-full">
-            {sideMenus.map(
-              (item, index) =>
-                (item.visibility?.includes(userrole?.toLowerCase()) ||
-                  item.visibility?.includes("all")) && (
-                  <li key={item.id}>
-                    <Link
-                      href={item.link || "#"}
-                      className={`flex items-center p-2 rounded-lg transition-colors duration-200 ${
-                        pathname === item.link
-                          ? "bg-gray-700 text-white"
-                          : "text-gray-400 hover:bg-gray-700 hover:text-white"
-                      } ${
-                        item.name === "Log out"
-                          ? "hover:bg-red-600 hover:text-white"
-                          : ""
-                      }`}
-                      onClick={() => sideHandler(index)}
-                    >
-                      <span className="text-xl mr-3">{item.icon}</span>
-                      {sidePanelStat && (
-                        <span className="flex-1 capitalize whitespace-nowrap">
-                          {item.name}
-                        </span>
-                      )}
-                      {sidePanelStat && item.notifications && (
-                        <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-gray-900 bg-blue-200 rounded-full">
-                          {item.notifications}
-                        </span>
-                      )}
-                      {sidePanelStat && item.nested && (
-                        <FaChevronDown
-                          className={`ml-auto transition-transform duration-200 ${
-                            currentIndex === index ? "transform rotate-180" : ""
-                          }`}
-                        />
-                      )}
-                    </Link>
-                    {sidePanelStat && currentIndex === index && item.nested && (
-                      <ul className="pl-6 mt-2 space-y-2">
-                        {item.nested.map(
-                          (subItem, subIndex) =>
-                            subItem.visibility?.includes(
-                              userrole.toLowerCase()
-                            ) && (
-                              <li key={subIndex}>
-                                <Link
-                                  href={subItem.link || "#"}
-                                  className={`block p-2 rounded-lg transition-colors duration-200 ${
-                                    pathname === subItem.link
-                                      ? "bg-gray-700 text-white"
-                                      : "text-gray-400 hover:bg-gray-700 hover:text-white"
-                                  }`}
-                                >
-                                  {subItem.name}
-                                </Link>
-                              </li>
-                            )
-                        )}
-                      </ul>
+        <nav className="flex-grow my-2">
+          <ul className="space-y-2 px-2">
+            {sideMenus.map((item, index) =>
+              (item.visibility?.includes(userrole?.toLowerCase()) ||
+                item.visibility?.includes("all")) &&
+              item.name !== "Log out" ? (
+                <li key={item.id}>
+                  <Link
+                    href={item.link || "#"}
+                    className={`flex items-center p-2 rounded-lg transition-colors duration-200 ${
+                      pathname === item.link
+                        ? "bg-gray-700 text-white"
+                        : "text-gray-400 hover:bg-gray-700 hover:text-white"
+                    } ${loading && "opacity-60 cursor-not-allowed"}`}
+                    onClick={() => !loading && sideHandler(index)}
+                  >
+                    <span className="text-2xl">{item.icon}</span>
+                    {sidePanelStat && (
+                      <span className="flex-1 ml-3 text-xs capitalize whitespace-nowrap">
+                        {item.name}
+                      </span>
                     )}
-                  </li>
-                )
+                    {sidePanelStat && item.notifications && (
+                      <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-gray-900 bg-blue-200 rounded-full">
+                        {item.notifications}
+                      </span>
+                    )}
+                    {sidePanelStat && item.nested && (
+                      <FaChevronDown
+                        className={`ml-auto transition-transform duration-200 ${
+                          currentIndex === index ? "transform rotate-180" : ""
+                        }`}
+                      />
+                    )}
+                  </Link>
+                  {sidePanelStat && currentIndex === index && item.nested && (
+                    <ul className="pl-6 mt-2 space-y-2">
+                      {item.nested.map((subItem, subIndex) =>
+                        subItem.visibility?.includes(userrole.toLowerCase()) ? (
+                          <li key={subIndex}>
+                            <Link
+                              href={subItem.link || "#"}
+                              className={`block p-2 text-xs rounded-lg transition-colors duration-200 ${
+                                pathname === subItem.link
+                                  ? "bg-gray-700 text-white"
+                                  : "text-gray-400 hover:bg-gray-700 hover:text-white"
+                              }`}
+                            >
+                              {subItem.name}
+                            </Link>
+                          </li>
+                        ) : null
+                      )}
+                    </ul>
+                  )}
+                </li>
+              ) : null
             )}
           </ul>
         </nav>
+
+        <div className="mt-auto p-2">
+          <ul className="space-y-2">
+            <li
+              onClick={window.document.body.classList.toggle(
+                "right-bar-enabled"
+              )}
+              className={`rounded-lg ${
+                pathname === "/settings" && !loading
+                  ? "bg-gray-700 text-white"
+                  : "text-gray-400 hover:bg-gray-700 hover:text-white"
+              }`}
+            >
+              <Link
+                href=""
+                onClick={(e) => e.preventDefault()}
+                className={`flex text-inherit items-center p-2 ml-3 transition-colors duration-200 ${
+                  loading && "opacity-60 cursor-not-allowed"
+                }`}
+              >
+                <Cog6ToothIcon className="w-6" />
+                {sidePanelStat && (
+                  <span className="flex-1 ml-3 text-xs capitalize whitespace-nowrap">
+                    Settings
+                  </span>
+                )}
+              </Link>
+            </li>
+            <li
+              onClick={handleLogout}
+              className={`rounded-lg text-gray-400 hover:bg-gray-700 ${
+                !loading && "hover:text-white"
+              }`}
+            >
+              <Link
+                href=""
+                onClick={(e) => e.preventDefault()}
+                className={`flex text-inherit items-center p-2 ml-3 transition-colors duration-200 ${
+                  loading && "opacity-60 cursor-not-allowed"
+                }`}
+              >
+                <ArrowLeftStartOnRectangleIcon className="w-6" />
+                {sidePanelStat && (
+                  <span className="flex-1 ml-3 text-xs capitalize whitespace-nowrap">
+                    Log out
+                  </span>
+                )}
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </Menu>
   );
