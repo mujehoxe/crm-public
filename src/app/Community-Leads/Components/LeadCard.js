22f98e39980b76ca75b818cd5ba0e636d5878e82
mujehoxe <mujehoxe@gmail.com>
2024-09-07 15:15:03 +0100

    lead card selection working

--- src/app/Community-Leads/Components/LeadCard.js
+++ src/app/Community-Leads/Components/LeadCard.js
@@ -6,7 +6,7 @@ import {FaPhone, FaWhatsapp} from "react-icons/fa6";
 import {Select} from "antd";
 import axios from "axios";
 import {toast} from "react-toastify";
-import {CheckIcon, PencilIcon} from "@heroicons/react/24/solid";
+import {CheckCircleIcon, PencilIcon} from "@heroicons/react/24/solid";
 import UpdateDescriptionInput from './UpdateDescriptionInput';
 import EditableSpan from "@/app/Community-Leads/Components/EditableSpan";
 
@@ -110,78 +110,82 @@ export default function LeadCard({
 	}
 
 	return <li
-		className={`relative col-span-1 divide-y divide-gray-200 h-min rounded-lg bg-white shadow hover:shadow-lg ${selectedLeads.includes(lead) ? "border-blue-400" : "hover:border-gray-400"}`}
+		className={`relative select-none col-span-1  overflow-shown border hover:border-gray-400 
+			h-min rounded-lg bg-white
+			${selectedLeads.includes(lead) ? "ring-2 ring-blue-400" : "shadow"}`}
 		ref={divRef}
 		onClick={(e) => handleCardClick(lead, e)}
 	>
-		{selectedLeads.includes(lead) &&
-			<div className="absolute -top-2 -left-2 z-10 text-xl">
-				<CheckIcon className="text-blue-600"/>
-			</div>}
 
-
-		<div
-			className="relative w-full items-center justify-between space-y-4 p-3">
-			<h3
-				className="truncate text-base font-medium text-gray-900">{lead.Name}</h3>
-			<div className="flex-shrink-0 flex items-center space-x-2">
+		{selectedLeads.includes(lead) && (
+			<div className="absolute -top-2 -left-2 z-50 text-xl">
+				<CheckCircleIcon className="text-blue-600 w-6 h-6"/>
+			</div>
+		)}
+		<div className="divide-y divide-gray-200">
+			<div
+				className="relative w-full items-center justify-between space-y-4 p-3">
+				<h3
+					className="truncate text-base font-medium text-gray-900">{lead.Name}</h3>
+				<div className="flex-shrink-0 flex items-center space-x-2">
 					<span
 						className="text-sm  text-gray-500">Assigned to:{" "}
 						<span className="font-medium text-gray-900">
 							{lead?.Assigned?.username}
 						</span>
 					</span>
-				{lead?.Assigned?.Avatar ?
-					<img className="h-6 w-6 rounded-full object-cover"
-							 src={`${process.env.NEXT_PUBLIC_BASE_URL || ""}${lead?.Assigned.Avatar}`}
-							 alt=""/> : <FaRegUserCircle className="h-6 w-6 text-gray-300"
-																					 aria-hidden="true"/>}
+					{lead?.Assigned?.Avatar ?
+						<img className="h-6 w-6 rounded-full object-cover"
+								 src={`${process.env.NEXT_PUBLIC_BASE_URL || ""}${lead?.Assigned.Avatar}`}
+								 alt=""/> : <FaRegUserCircle className="h-6 w-6 text-gray-300"
+																						 aria-hidden="true"/>}
+				</div>
 			</div>
-		</div>
 
-		<div className="absloute bottom-0 top-auto p-3 space-y-2">
-			<div className="flex space-x-2">
-				<Select
-					mode="single"
-					allowClear
-					style={{width: '150px'}}
-					defaultValue={lead.LeadStatus?.Status}
-					onClick={e => e.stopPropagation()}
-					onChange={(_, option) => selectChange(option, 'LeadStatus')}
-					options={statusOptions}
-					option
-					placeholder={"Status"}
-				/>
-				<Select
-					mode="single"
-					allowClear
-					style={{width: '150px'}}
-					defaultValue={lead.Source?.Source}
-					onClick={e => e.stopPropagation()}
-					onChange={(_, option) => selectChange(option, 'Source')}
-					options={sourceOptions}
-					option
-					placeholder={"Source"}
-				/>
-			</div>
-			<div className="flex space-x-1 items-center">
+			<div className="absloute bottom-0 top-auto p-3 space-y-2">
+				<div className="flex space-x-2">
+					<Select
+						mode="single"
+						allowClear
+						style={{width: '150px'}}
+						defaultValue={lead.LeadStatus?.Status}
+						onClick={e => e.stopPropagation()}
+						onChange={(_, option) => selectChange(option, 'LeadStatus')}
+						options={statusOptions}
+						option
+						placeholder={"Status"}
+					/>
+					<Select
+						mode="single"
+						allowClear
+						style={{width: '150px'}}
+						defaultValue={lead.Source?.Source}
+						onClick={e => e.stopPropagation()}
+						onChange={(_, option) => selectChange(option, 'Source')}
+						options={sourceOptions}
+						option
+						placeholder={"Source"}
+					/>
+				</div>
+				<div className="flex space-x-1 items-center">
 				<span
 					className="text-sm font-medium text-gray-500">Marketing Tag:</span>
-				<EditableSpan
-					content={lead?.marketingtags?.Tag || 'No Tag'}
-					onBlur={(tag)=>tagChange(tag, 'marketingtags')}
-				/>
-			</div>
-			<div className="flex space-x-1 items-center">
-				<span className="text-sm font-medium text-gray-500">DLD Tag:</span>
-				<EditableSpan
-					content={lead?.tags?.Tag || 'No Tag'}
-					onBlur={(tag)=>tagChange(tag, 'tags')}
-				/>
-			</div>
-			<div className="space-y-1">
-				<span className="text-sm font-medium text-gray-500">Description:</span>
-				<div className="flex justify-between space-x-1 items-start">
+					<EditableSpan
+						content={lead?.marketingtags?.Tag || 'No Tag'}
+						onBlur={(tag) => tagChange(tag, 'marketingtags')}
+					/>
+				</div>
+				<div className="flex space-x-1 items-center">
+					<span className="text-sm font-medium text-gray-500">DLD Tag:</span>
+					<EditableSpan
+						content={lead?.tags?.Tag || 'No Tag'}
+						onBlur={(tag) => tagChange(tag, 'tags')}
+					/>
+				</div>
+				<div className="space-y-1">
+					<span
+						className="text-sm font-medium text-gray-500">Description:</span>
+					<div className="flex justify-between space-x-1 items-start">
 					<span
 						contentEditable={isDescriptionInput && !loading}
 						onBlur={({target: {innerText}}) => {
@@ -191,90 +195,91 @@ export default function LeadCard({
 					>
 							{lead.Description || 'No Description'}
 					</span>
-					{!isDescriptionInput &&
-						<PencilIcon
-							className="min-w-6 min-h-6 w-6 h-6 text-green-400 bg-green-100 rounded-full p-1 cursor-pointer hover:bg-green-200"
-							onClick={e => {
+						{!isDescriptionInput &&
+							<PencilIcon
+								className="min-w-6 min-h-6 w-6 h-6 text-green-400 bg-green-100 rounded-full p-1 cursor-pointer hover:bg-green-200"
+								onClick={e => {
+									e.stopPropagation();
+									setIsDescriptionInput(true)
+								}}
+							/>
+						}
+					</div>
+				</div>
+			</div>
+
+			<div className="mt-auto">
+				<div className="-mt-px flex divide-x divide-gray-200">
+					<div className="flex w-0 flex-1">
+						<button
+							onClick={(e) => {
 								e.stopPropagation();
-								setIsDescriptionInput(true)
+								setShowContact(!showContact);
 							}}
-						/>
-					}
+							className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg py-3 text-sm font-semibold text-gray-900"
+						>
+							<FaPhone className="h-5 w-5 text-gray-400" aria-hidden="true"/>
+							Contact
+						</button>
+					</div>
+					<div className="-ml-px flex w-0 flex-1">
+						<button
+							onClick={detailsButtonClick}
+							className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg py-3 text-sm font-semibold text-gray-900"
+						>
+							<IoIosInformationCircle className="h-5 w-5 text-gray-400"
+																			aria-hidden="true"/>
+							Details
+						</button>
+					</div>
 				</div>
 			</div>
+
+			<AnimatePresence>
+				{showContact && <motion.div
+					variants={container}
+					initial="hidden"
+					animate="show"
+					exit="hidden"
+					className="flex items-center justify-center px-6 py-3 space-x-2"
+				>
+					<motion.div variants={listItem}
+											className="flex items-center space-x-2">
+						<a href={`tel:${lead.Phone}`}
+							 className="inline-flex items-center rounded-full bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+							<FaPhone className="mr-1"/> {lead.Phone}
+						</a>
+						{lead.AltPhone && (<a href={`tel:${lead.AltPhone}`}
+																	className="inline-flex items-center rounded-full bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-700/10">
+							<FaPhone className="mr-1"/> {lead.AltPhone}
+						</a>)}
+					</motion.div>
+					<motion.div variants={listItem}
+											className="flex items-center space-x-2">
+						<a
+							target="_blank"
+							href={`https://wa.me/${encodeURIComponent(lead.Phone)}?text=${encodeURIComponent("Your custom message here")}`}
+							className="inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-700/10"
+						>
+							<FaWhatsapp className="mr-1"/> WhatsApp
+						</a>
+						{lead.AltPhone && (<a
+							target="_blank"
+							href={`https://wa.me/${encodeURIComponent(lead.AltPhone)}?text=${encodeURIComponent("Your custom message here")}`}
+							className="inline-flex items-center rounded-full bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-700/10"
+						>
+							<FaWhatsapp className="mr-1"/> Alt WhatsApp
+						</a>)}
+					</motion.div>
+				</motion.div>}
+			</AnimatePresence>
+
+			<UpdateDescriptionInput
+				isUpdateDescriptionInput={isUpdateDescriptionInput}
+				loading={loading}
+				setUpdateBody={setUpdateBody}
+				handleUpdateSubmit={handleUpdateSubmit}
+			/>
 		</div>
-
-		<div className="mt-auto">
-			<div className="-mt-px flex divide-x divide-gray-200">
-				<div className="flex w-0 flex-1">
-					<button
-						onClick={(e) => {
-							e.stopPropagation();
-							setShowContact(!showContact);
-						}}
-						className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg py-3 text-sm font-semibold text-gray-900"
-					>
-						<FaPhone className="h-5 w-5 text-gray-400" aria-hidden="true"/>
-						Contact
-					</button>
-				</div>
-				<div className="-ml-px flex w-0 flex-1">
-					<button
-						onClick={detailsButtonClick}
-						className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg py-3 text-sm font-semibold text-gray-900"
-					>
-						<IoIosInformationCircle className="h-5 w-5 text-gray-400"
-																		aria-hidden="true"/>
-						Details
-					</button>
-				</div>
-			</div>
-		</div>
-
-		<AnimatePresence>
-			{showContact && <motion.div
-				variants={container}
-				initial="hidden"
-				animate="show"
-				exit="hidden"
-				className="flex items-center justify-center px-6 py-3 space-x-2"
-			>
-				<motion.div variants={listItem}
-										className="flex items-center space-x-2">
-					<a href={`tel:${lead.Phone}`}
-						 className="inline-flex items-center rounded-full bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
-						<FaPhone className="mr-1"/> {lead.Phone}
-					</a>
-					{lead.AltPhone && (<a href={`tel:${lead.AltPhone}`}
-																className="inline-flex items-center rounded-full bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-700/10">
-						<FaPhone className="mr-1"/> {lead.AltPhone}
-					</a>)}
-				</motion.div>
-				<motion.div variants={listItem}
-										className="flex items-center space-x-2">
-					<a
-						target="_blank"
-						href={`https://wa.me/${encodeURIComponent(lead.Phone)}?text=${encodeURIComponent("Your custom message here")}`}
-						className="inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-700/10"
-					>
-						<FaWhatsapp className="mr-1"/> WhatsApp
-					</a>
-					{lead.AltPhone && (<a
-						target="_blank"
-						href={`https://wa.me/${encodeURIComponent(lead.AltPhone)}?text=${encodeURIComponent("Your custom message here")}`}
-						className="inline-flex items-center rounded-full bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-700/10"
-					>
-						<FaWhatsapp className="mr-1"/> Alt WhatsApp
-					</a>)}
-				</motion.div>
-			</motion.div>}
-		</AnimatePresence>
-
-		<UpdateDescriptionInput
-			isUpdateDescriptionInput={isUpdateDescriptionInput}
-			loading={loading}
-			setUpdateBody={setUpdateBody}
-			handleUpdateSubmit={handleUpdateSubmit}
-		/>
 	</li>;
 };
