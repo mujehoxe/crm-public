1c078e8be91aee6186aa727cfb8f36d2e237c26d
mujehoxe <mujehoxe@gmail.com>
2024-10-02 17:42:43 +0100

    dynamic notification rout

--- src/app/api/sse-notifications/route.js
+++ src/app/api/sse-notifications/route.js
@@ -2,6 +2,8 @@ import { addSSEConnection, removeSSEConnection } from "@/models/Activity";
 import { NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req) {
   const connectionId = uuidv4();
   console.log(`New SSE connection attempt. ID: ${connectionId}`);
