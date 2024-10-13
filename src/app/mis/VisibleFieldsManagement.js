c1fe249ae8db2851a9a764c97fc6c5ddf863b89a
mujehoxe <mujehoxe@gmail.com>
2024-10-13 10:51:06 +0100

    fix fields not loading in fields managment table

--- src/app/mis/VisibleFieldsManagement.js
+++ src/app/mis/VisibleFieldsManagement.js
@@ -136,78 +136,79 @@ export default function VisibleFieldsManagement({ fields }) {
               </tr>
             </thead>
             <tbody className="divide-y-2 divide-gray-200">
-              {clientFields?.map((field, fieldIndex) => (
-                <tr
-                  key={`${field.name}`}
-                  className={`${
-                    fieldIndex % 2 === 0 ? "bg-gray-50" : "bg-white"
-                  }`}
-                >
-                  <td className="capitalize border-r border-gray-400 hover:bg-miles-100 transition-all text-nowrap py-4 px-2 text-sm text-gray-700 font-medium">
-                    {field.name.replaceAll("_", " ")}
-                  </td>
-                  {roles.map((role) => (
-                    <td
-                      key={`${field.name}-${role}`}
-                      className="group-hover:bg-miles-100 transition-all text-center"
-                    >
-                      <div className="inline-flex items-center">
-                        <label className="flex items-center cursor-pointer relative">
-                          <input
-                            type="checkbox"
-                            disabled={role === "superAdmin"}
-                            checked={field.visibleTo
-                              .map((v) => v._id)
-                              .includes(role._id)}
-                            onChange={() =>
-                              handleFieldVisibilityChange(field.name, role)
-                            }
-                            className={`peer h-4 w-4 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-gray-300 disabled:bg-gray-300 disabled:border-gray-300 ${
-                              field.visibleTo
-                                .map((v) => v._id)
-                                .includes(role._id) !==
-                              (clientInitialFields
-                                .find((f) => f._id === field._id)
-                                ?.visibleTo.some((v) => v._id === role._id) ||
-                                false)
-                                ? "bg-green-600 border-2 border-green-600"
-                                : field.visibleTo
-                                    .map((v) => v._id)
-                                    .includes(role._id)
-                                ? "checked:bg-miles-600 checked:border-miles-600"
-                                : ""
-                            }`}
-                          />
-                          <span
-                            className={`absolute text-white ${
-                              field.visibleTo
-                                .map((v) => v._id)
-                                .includes(role._id)
-                                ? "opacity-100"
-                                : "opacity-0"
-                            } top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`}
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="h-3.5 w-3.5"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                              stroke="currentColor"
-                              strokeWidth="1"
-                            >
-                              <path
-                                fillRule="evenodd"
-                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                clipRule="evenodd"
-                              />
-                            </svg>
-                          </span>
-                        </label>
-                      </div>
+              {clientFields &&
+                clientFields?.map((field, fieldIndex) => (
+                  <tr
+                    key={`${field.name}`}
+                    className={`${
+                      fieldIndex % 2 === 0 ? "bg-gray-50" : "bg-white"
+                    }`}
+                  >
+                    <td className="capitalize border-r border-gray-400 hover:bg-miles-100 transition-all text-nowrap py-4 px-2 text-sm text-gray-700 font-medium">
+                      {field.name.replaceAll("_", " ")}
                     </td>
-                  ))}
-                </tr>
-              ))}
+                    {roles.map((role) => (
+                      <td
+                        key={`${field.name}-${role}`}
+                        className="group-hover:bg-miles-100 transition-all text-center"
+                      >
+                        <div className="inline-flex items-center">
+                          <label className="flex items-center cursor-pointer relative">
+                            <input
+                              type="checkbox"
+                              disabled={role === "superAdmin"}
+                              checked={field.visibleTo
+                                .map((v) => v._id)
+                                .includes(role._id)}
+                              onChange={() =>
+                                handleFieldVisibilityChange(field.name, role)
+                              }
+                              className={`peer h-4 w-4 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-gray-300 disabled:bg-gray-300 disabled:border-gray-300 ${
+                                field.visibleTo
+                                  .map((v) => v._id)
+                                  .includes(role._id) !==
+                                (clientInitialFields
+                                  .find((f) => f._id === field._id)
+                                  ?.visibleTo.some((v) => v._id === role._id) ||
+                                  false)
+                                  ? "bg-green-600 border-2 border-green-600"
+                                  : field.visibleTo
+                                      .map((v) => v._id)
+                                      .includes(role._id)
+                                  ? "checked:bg-miles-600 checked:border-miles-600"
+                                  : ""
+                              }`}
+                            />
+                            <span
+                              className={`absolute text-white ${
+                                field.visibleTo
+                                  .map((v) => v._id)
+                                  .includes(role._id)
+                                  ? "opacity-100"
+                                  : "opacity-0"
+                              } top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2`}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-3.5 w-3.5"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                stroke="currentColor"
+                                strokeWidth="1"
+                              >
+                                <path
+                                  fillRule="evenodd"
+                                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                  clipRule="evenodd"
+                                />
+                              </svg>
+                            </span>
+                          </label>
+                        </div>
+                      </td>
+                    ))}
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
