a0fa42a062470bd24426484ecea5613507a30114
mujehoxe <mujehoxe@gmail.com>
2024-10-14 14:23:54 +0100

    continue rename serverside

--- src/app/api/Lead/get/route.js
+++ src/app/api/Lead/get/route.js
@@ -1,11 +1,8 @@
-import getDataFromToken from "@/helpers/getDataFromtoken";
-import { NextRequest, NextResponse } from "next/server";
-import Leads from "@/models/Leads";
 import connect from "@/dbConfig/dbConfig";
+import Leads from "@/models/Leads";
 import logger from "@/utils/logger";
-import { parseISO, isValid, format } from "date-fns"; // Import parseISO and format from date-fns
 import moment from "moment";
-import SourceModel from "@/models/Source";
+import { NextResponse } from "next/server";
 
 connect();
 
@@ -15,28 +12,20 @@ export async function GET(request) {
     const page = parseInt(searchParams.get("page")) || 1;
     const limit = parseInt(searchParams.get("limit")) || 50;
     const searchterm = searchParams.get("searchterm") || "";
-    const selectedValuesString = searchParams.get("selectedValues") || "";
-    const selectedValuesString2 = searchParams.get("selectedValues2") || "";
-    const selectedValuesString3 = searchParams.get("selectedValues3") || "";
-    const selectedValuesString4 = searchParams.get("selectedTag") || "";
+    const selectedAgentsString = searchParams.get("selectedAgents") || "";
+    const selectedSourcesString = searchParams.get("selectedStatuses") || "";
+    const selectedStatusesString = searchParams.get("selectedSources") || "";
+    const selectedTagsString = searchParams.get("selectedTags") || "";
     const dateString = searchParams.get("date") || "";
     const skip = (page - 1) * limit;
-    const selectedValues = selectedValuesString
-      ? selectedValuesString.split(",")
-      : [];
-    const selectedValues2 = selectedValuesString2
-      ? selectedValuesString2.split(",")
-      : [];
-    const selectedValues3 = selectedValuesString3
-      ? selectedValuesString3.split(",")
-      : [];
-    const selectedValues4 = selectedValuesString4
-      ? selectedValuesString4.split(",")
-      : [];
+
+    const selectedAgents = selectedAgentsString.split(",").filter(Boolean);
+    const selectedStatuses = selectedSourcesString.split(",").filter(Boolean);
+    const selectedSources = selectedStatusesString.split(",").filter(Boolean);
+    const selectedTags = selectedTagsString.split(",").filter(Boolean);
 
     let query = {};
 
-    // Build the base query
     if (searchterm) {
       let searchConditions;
 
@@ -49,20 +38,15 @@ export async function GET(request) {
       query.$or = searchConditions;
     }
 
-    // Add selectedValues and selectedValues2 conditions
-    if (selectedValues.length > 0) {
-      query.Assigned = { $in: selectedValues };
-    }
+    if (selectedAgents.length > 0) query.Assigned = { $in: selectedAgents };
 
-    if (selectedValues2.length > 0) {
-      query.LeadStatus = { $in: selectedValues2 };
-    }
-    if (selectedValues3.length > 0) {
-      query.Source = { $in: selectedValues3 };
-    }
-    if (selectedValues4.length > 0) {
-      query.tags = { $in: selectedValues4 };
-    }
+    if (selectedStatuses.length > 0)
+      query.LeadStatus = { $in: selectedStatuses };
+
+    if (selectedSources.length > 0) query.Source = { $in: selectedSources };
+
+    if (selectedTags.length > 0) query.tags = { $in: selectedTags };
+
     if (dateString != "" && dateString != ",") {
       const dates = dateString.split(",");
 
